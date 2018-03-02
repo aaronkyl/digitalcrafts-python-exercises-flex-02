cff9d92f39f2e6cd8decfe95d09b199f0c29be29
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-03-02 06:38:10 +0000

    Step 4 of Guess the Number game assignment

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,20 @@
 import random
 secret_number = random.randint(1, 10)
+guesses_left = 5
+guess = 0
 
 print("I am thinking of a number between 1 and 10.")
 
-guess = int(input("Whats the number? "))
-
-while guess != secret_number:
+while guesses_left:
+    guesses_left -= 1
+    guess = int(input("What's the number? "))
     if guess < secret_number:
         print("{guess} is too low.".format(guess=guess))
-    else:
+    elif guess > secret_number:
         print("{guess} is too high.".format(guess=guess))
-    guess = int(input("Whats the number? "))
-    
-print("Yes! You win!")
+    else:
+        print("Yes! You win!")
+        break
+
+if not guesses_left:
+    print("You ran out of guesses!")
