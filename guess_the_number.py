bfe4d02d5ff3fb48f755e0dccb450d4b259d0517
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-03-02 06:45:45 +0000

    Add bonus play again option

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,20 +1,30 @@
 import random
-secret_number = random.randint(1, 10)
-guesses_left = 5
-guess = 0
 
-print("I am thinking of a number between 1 and 10.")
+replay = 'Y'
 
-while guesses_left:
-    guesses_left -= 1
-    guess = int(input("What's the number? "))
-    if guess < secret_number:
-        print("{guess} is too low.".format(guess=guess))
-    elif guess > secret_number:
-        print("{guess} is too high.".format(guess=guess))
-    else:
-        print("Yes! You win!")
-        break
-
-if not guesses_left:
-    print("You ran out of guesses!")
+while replay == 'Y':
+    secret_number = random.randint(1, 10)
+    guesses_left = 5
+    guess = 0
+    
+    print("I am thinking of a number between 1 and 10.")
+    
+    while guesses_left:
+        guesses_left -= 1
+        guess = int(input("What's the number? "))
+        if guess < secret_number:
+            print("{guess} is too low.".format(guess=guess))
+        elif guess > secret_number:
+            print("{guess} is too high.".format(guess=guess))
+        else:
+            print("Yes! You win!")
+            break
+    
+    if not guesses_left:
+        print("You ran out of guesses!")
+    
+    replay = ""
+    while replay not in ['Y', 'N']:
+        replay = input("Do you want to play again? (Y or N) ").upper()
+        
+print("Bye!")
