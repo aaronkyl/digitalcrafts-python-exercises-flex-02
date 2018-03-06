708f1dbb2fef6a3553d9b769a8a47c05f0580f47
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-03-06 22:41:56 +0000

    matrix multiplication

--- week1/sat/matrix_multiplication.py
+++ week1/sat/matrix_multiplication.py
@@ -4,12 +4,19 @@ matrix1 = [[2, -2], [5, 3]]
 matrix2 = [[-1, 4], [7, -6]]
 result = []
 
-array_dimension = len(matrix1)
+matrix1_rows = len(matrix1)
+matrix1_cols = len(matrix1[1])
+matrix2_rows = len(matrix2)
+matrix2_cols = len(matrix2[1])
 
-for i in range(0, array_dimension):
+for i in range(0, matrix1_rows):
     result.append([])
-    for j in range(0, array_dimension):
-        for k in range(0, array_dimension):
-            result[i].append(matrix1[i][j] * matrix2[i][i] + matrix1[i][j] * matrix2[j][i])
-        
+    for j in range(0, matrix2_cols):
+        result[i].append(0)
+        for k in range(0, matrix1_cols):
+            result[i][j] += matrix1[i][k] * matrix2[k][j]
+            k += 1
+        j += 1
+    i += 1
+
 print(result)
