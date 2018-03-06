06c6756d717cf029bde8bc49e46b87cc39ef4c93
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-03-06 16:56:12 +0000

    complete caesar cipher

--- week1/sat/caesar_cipher.py
+++ week1/sat/caesar_cipher.py
@@ -1,1 +1,30 @@
-# TODO 
+# initialize variables
+plaintext = ''
+key = -1
+
+# get plaintext and cipher value from user
+while plaintext == '':
+    plaintext = input("Please enter your text: ")
+while key < 0:
+    key = int(input("Please enter a non-negative integer key: "))
+
+# prepare output line
+print("ciphertext: ", end='')
+
+# iterate through plaintext
+for char in plaintext:
+    if char.isalpha():
+        # flag to record case of current character
+        if char.isupper():
+            a = 'A'
+        else:
+            a = 'a'
+        # encode current alpha character and print it
+        print(chr((((ord(char) - ord(a)) + key) % 26) + ord(a)), end='')
+    else:
+        # print non-alpha character
+        print(char, end='')
+
+# print newline at end
+print('')
+        
